14b0060af77e25d33fae340924cfe7ea4af871a0
Vasily Perekhrest <perekhrest-vasily@mail.ru>
2022-07-12 19:49:43 +0700

    Added zsh script for installing plugins

---
+++ install-zsh-plugins.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+# zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
+source ~/.zshrc
