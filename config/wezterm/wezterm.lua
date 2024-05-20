fb203d86c79c80c4993822b083802aef28bf57ef
Vasily Perekhrest <perekhrest-vasily@mail.ru>
2024-05-20 23:02:41 +0700

    Added bindings for movement

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -37,9 +37,8 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
         {Text=SOLID_RIGHT_ARROW},
         {Attribute={Intensity="Normal"}},
     }
-end)
-
-
+    end
+)
 
 local config = wezterm.config_builder()
 
@@ -53,55 +52,81 @@ config.cursor_blink_rate = 500
 config.colors = {
     tab_bar = {
         background = "#121212",
-        new_tab = {bg_color = "#121212", fg_color = "#FCE8C3", intensity = "Bold"},
-        new_tab_hover = {bg_color = "#121212", fg_color = "#61afef", intensity = "Bold"},
+        new_tab = {
+            bg_color = "#121212",
+            fg_color = "#FCE8C3",
+            intensity = "Bold",
+        },
+        new_tab_hover = {
+            bg_color = "#121212",
+            fg_color = "#61afef",
+            intensity = "Bold",
+        },
     },
     cursor_bg = '#61afef',
     cursor_fg = '#61afef',
-
     foreground = '#abb2bf',
     background = '#21252b',
-
     selection_fg = '#abb2bf',
     selection_bg = '#323844',
-
     ansi = {
-    '#21252b',
-	'#e06c75',
-	'#98c379',
-	'#e5c07b',
-	'#61afef',
-	'#c678dd',
-	'#56b6c2',
-	'#abb2bf',
+        '#21252b',
+        '#e06c75',
+	    '#98c379',
+	    '#e5c07b',
+	    '#61afef',
+	    '#c678dd',
+	    '#56b6c2',
+	    '#abb2bf',
     },
     brights = {
         '#767676',
-	'#e06c75',
-	'#98c379',
-	'#e5c07b',
-	'#61afef',
-	'#c678dd',
-	'#56b6c2',
-	'#abb2bf',
+	    '#e06c75',
+	    '#98c379',
+	    '#e5c07b',
+	    '#61afef',
+	    '#c678dd',
+	    '#56b6c2',
+	    '#abb2bf',
     },
-
 }
 
 config.font = wezterm.font "FiraCodeNerdFont"
 config.font_size = 11.0
-
 config.window_padding = {
     left = 4,
     right = 4,
     bottom = 2,
-    top = 2, 
+    top = 2,
 }
-
 config.window_background_opacity = 0.97
 config.hide_tab_bar_if_only_one_tab = false
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.tab_max_width = 60
+
+
+config.keys = {
+    {
+        key = 'LeftArrow',
+        mods = 'CTRL',
+        action = wezterm.action.MoveTabRelative(-1),
+    },
+    {
+        key = 'RightArrow',
+        mods = 'CTRL',
+        action = wezterm.action.MoveTabRelative(1),
+    },
+    {
+        key = 't',
+        mods = 'CTRL',
+        action = wezterm.action.SpawnTab "CurrentPaneDomain",
+    },
+    {
+        key = 'w',
+        mods = 'CTRL',
+        action = wezterm.action.CloseCurrentTab {confirm=true},
+    },
+}
 
 return config
